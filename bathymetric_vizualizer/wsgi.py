c93d53cd733b1bf0135baff2a6b757f17efd8130
matthew-buglass <buglassmatthew@gmail.com>
2024-01-14 13:22:00 -0600

    Added django infrastructure

---
+++ bathymetric_vizualizer/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bathymetric_vizualizer project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bathymetric_vizualizer.settings')
+
+application = get_wsgi_application()
