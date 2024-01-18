d2fd066393444153a492146767c90d53c7857fe4
matthew-buglass <buglassmatthew@gmail.com>
2024-01-17 19:13:28 -0600

    got orbit controls mostly working

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,11 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const width = window.innerWidth, height = window.innerHeight;
 
 // init
 
-const camera = new THREE.PerspectiveCamera( 100, width / height, 0.01, 100 );
+const camera = new THREE.PerspectiveCamera( 50, width / height, 0.01, 100 );
 camera.position.z = 10;
 
 const scene = new THREE.Scene();
@@ -13,11 +14,9 @@ let mesh = new THREE.Mesh();
 const renderer = new THREE.WebGLRenderer( { antialias: true } );
 renderer.setSize( width, height );
 
-// const controls = new OrbitControls( camera, renderer.domElement );
-// controls.target.set( 0, 0.5, 0 );
-// controls.update();
-// controls.enablePan = false;
-// controls.enableDamping = true;
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.update();
+animate();
 
 export function addData( vertices, face_indices ) {
 	document.getElementById("render-region").appendChild( renderer.domElement );
@@ -31,5 +30,31 @@ export function addData( vertices, face_indices ) {
 	const material = new THREE.MeshNormalMaterial();
 	mesh = new THREE.Mesh( geometry, material );
 	scene.add( mesh );
+
+	render()
+}
+
+function onWindowResize() {
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function animate() {
+
+	requestAnimationFrame( animate );
+
+	controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
+
+	render();
+
+}
+
+function render() {
+
 	renderer.render( scene, camera );
+
 }
