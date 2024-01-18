04a4ca51a0ca056f957005af954f91dccc3cb93b
matthew-buglass <buglassmatthew@gmail.com>
2024-01-17 19:50:39 -0600

    added the lint badge to the README.md to make me feel like I'm actually doing something.

--- vis/utils.py
+++ vis/utils.py
@@ -1,15 +1,15 @@
-def flatten(list_of_lists: list[list], cast_to: type = None) -> list:
+def flatten(l: list[list], cast_to: type = None) -> list:
     """
     Flattens a 2D matrix into a vector
 
     Args:
-        list_of_lists: a 2D matrix
+        l: a 2D matrix
         cast_to: (OPTIONAL) a type of function to cast the type of the elements
 
     Returns:
         vec: the vector form of the 2D list with the elements of l[i] proceeded by the elements of l[i+1]
     """
     if cast_to is not None:
-        return [cast_to(i) for j in list_of_lists for i in j]
+        return [cast_to(i) for j in l for i in j]
     else:
-        return [i for j in list_of_lists for i in j]
+        return [i for j in l for i in j]
