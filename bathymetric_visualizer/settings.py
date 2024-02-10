03220841b86e86a1fc93f2225124b9f0433eb315
matthew-buglass <buglassmatthew@gmail.com>
2024-02-10 15:57:36 -0600

    re-arranged the app imports

--- bathymetric_visualizer/settings.py
+++ bathymetric_visualizer/settings.py
@@ -32,17 +32,17 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'daphne',
+    'channels',
+    'webpack_loader',
+    'rest_framework',
+    'corsheaders',
+    'vis',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'channels',
-    'webpack_loader',
-    'rest_framework',
-    'corsheaders',
-    'vis',
 ]
 
 MIDDLEWARE = [
