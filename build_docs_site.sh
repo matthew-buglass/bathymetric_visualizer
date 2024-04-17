90d7d860bcf3290b219da13d5b1cea8efcfc8146
matthew-buglass <buglassmatthew@gmail.com>
2024-04-17 16:35:55 -0600

    added the doc packages and started building the docs generation script

---
+++ build_docs_site.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Create missing source files
+for filename in vis/*.py; do
+  if [[ "$filename" != *"__init__"* ]]; then
+    file_no_py="${filename%.*}"
+    md_filename="$file_no_py.md"
+    content="::: $(echo "$file_no_py" | tr / .)"
+
+    echo $md_filename
+    echo $content
+
+    if ! test -f docs_source/"$md_filename"; then
+      touch docs_source/"$md_filename"
+      echo "$content" > docs_source/"$md_filename"
+    fi
+  fi
+done
+
+#cp README.md docs_source/index.md
+#cp -r readme_imgs docs_source
+#mkdocs build --clean
+
