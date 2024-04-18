353de2f09329ec29b55935233f98ed82ff08dc14
matthew-buglass <buglassmatthew@gmail.com>
2024-04-18 13:29:54 -0600

    got auto docs generation working

--- build_docs_site.sh
+++ build_docs_site.sh
@@ -7,17 +7,15 @@ for filename in vis/*.py; do
     md_filename="$file_no_py.md"
     content="::: $(echo "$file_no_py" | tr / .)"
 
-    echo $md_filename
-    echo $content
-
     if ! test -f docs_source/"$md_filename"; then
+      mkdir -p "docs_source/$(dirname "$md_filename")"
       touch docs_source/"$md_filename"
       echo "$content" > docs_source/"$md_filename"
     fi
   fi
 done
 
-#cp README.md docs_source/index.md
+cp README.md docs_source/index.md
 #cp -r readme_imgs docs_source
-#mkdocs build --clean
+mkdocs build --clean
 
